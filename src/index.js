88a0f930943ff88cc6cc4b07931fd16e95da5178
mahmod258 <mahmod.rabee2007@gmail.com>
2022-04-12 00:23:22 +0300

    change path for fontawesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "../fontawesome/css/all.min.css";
+import "./css/app.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
