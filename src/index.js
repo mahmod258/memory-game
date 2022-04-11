7e5b6a92f8102db0970f2bab03039199188bd861
mahmod258 <mahmod.rabee2007@gmail.com>
2022-04-12 00:41:32 +0300

    nothing4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "../fontawesome/css/all.min.css";
-import "./css/app.css";
+import "./style/app.css";
+import "./fontawesome/css/all.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
